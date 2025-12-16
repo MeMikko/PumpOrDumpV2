8db699e21d8511599a6f6c2c395aad3cd7ded0d0
MeMikko <96080775+MeMikko@users.noreply.github.com>
2025-12-17 00:21:51 +0200

    Update DesktopNav.tsx

--- src/app/components/DesktopNav.tsx
+++ src/app/components/DesktopNav.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ConnectButton } from "@/components/ConnectButton";
+import { ConnectButton } from "./ConnectButton";
 
 export default function DesktopNav() {
   const pathname = usePathname();
