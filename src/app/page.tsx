354735ca8c051a0e8e06b658caf0e69a2fa92ec9
MeMikko <96080775+MeMikko@users.noreply.github.com>
2025-12-17 02:09:21 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useAccount, useWriteContract } from "wagmi";
-import { fetchBirdeyePrices } from "@/components/birdeye";
+import { fetchBirdeyePrices } from "../lib/birdeye";
 
 
 import {
