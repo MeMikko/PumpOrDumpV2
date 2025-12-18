5b868ae808136d1c7f7d219194cc63dccd3ce75f
MeMikko <96080775+MeMikko@users.noreply.github.com>
2025-12-18 18:19:31 +0200

    Update useSession.ts

--- src/lib/useSession.ts
+++ src/lib/useSession.ts
@@ -4,10 +4,15 @@ import { create } from "zustand";
 
 type SessionState = {
   signedIn: boolean;
+  ready: boolean;
   markSignedIn: () => void;
+  markReady: () => void;
 };
 
 export const useSession = create<SessionState>((set) => ({
   signedIn: false,
+  ready: false,
+
   markSignedIn: () => set({ signedIn: true }),
+  markReady: () => set({ ready: true }),
 }));
